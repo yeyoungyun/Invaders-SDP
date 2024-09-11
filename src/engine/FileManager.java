0801d58294bb1f70a1080ca06d7dbb15a3a3afa3
Choi-89 <wlghks3418@naver.com>
2024-09-11 17:50:05 +0900

    re: 게임 돌아가게 수정함

--- src/engine/FileManager.java
+++ src/engine/FileManager.java
@@ -68,7 +68,9 @@ public final class FileManager {
 	public void loadSprite(final Map<SpriteType, boolean[][]> spriteMap)
 			throws IOException {
 		InputStream inputStream = null;
-
+		/**--1-- 첫번째 변경점
+		 * res파일을 못찾는 것 같아서 res파일을 리소스파일로 만들어줌
+		 */
 		try {
 			inputStream = DrawManager.class.getClassLoader()
 					.getResourceAsStream("graphics");
@@ -116,8 +118,12 @@ public final class FileManager {
 
 		try {
 			// Font loading.
+			/**--1-- 첫번째 변경점
+			 * 1. res파일을 못찾는 것 같아서 res파일을 리소스파일로 만들어줌
+			 * 2. font파일이 없어서 github에 있는 폰트로 경로 설정 해줌
+			 */
 			inputStream = FileManager.class.getClassLoader()
-					.getResourceAsStream("font.ttf");
+					.getResourceAsStream("space_invaders.ttf");
 			font = Font.createFont(Font.TRUETYPE_FONT, inputStream).deriveFont(
 					size);
 		} finally {
